4e0efefce7e5e2bbe9b1b5a4d684e9e53118bb32
Janani Jade <ja2002.js@gmail.com>
2020-12-01 18:35:16 +0530

    Update Helloworldfinally.cpp

--- Helloworldfinally.cpp
+++ Helloworldfinally.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 int main()
 {
+    char Name[100];
     std::cout<<"Hello world for once and for all!";
+    std::cout<<"\nWhat's your name?";std::cin>>Name;
+    std::cout<<"Hello "<<Name<<"\nHave a great day!";    
     return 0;
 
 }
